9294ad2b722912e9e7b348ad5794f5ba8677ec4d
epan <epan616@gmail.com>
2023-02-21 23:12:29 -0500

    Add functions for loading questions and answers

--- docs/script.js
+++ docs/script.js
@@ -9,4 +9,61 @@ function countdown() {
     }, 1000)
 }
 
+var quizStartEl = document.getElementById("quizStart");
+var quizEl = document.getElementById("quiz");
+var quizFinishedEl = document.getElementById("quizFinished");
+var showHighScoresEl = document.getElementById("showHighScores");
+
+// Functions to clear Page Sections
+function clearQuizStart() {
+    quizStartEl.style.display = "none";
+}
+
+function clearQuiz() {
+    quizEl.style.display = "none";
+}
+
+function clearQuizFinished() {
+    quizFinishedEl.style.display = "none";
+}
+
+function clearShowHighScores() {
+    showHighScoresEl.style.display = "none";
+}
+
+var currentQuestion = 0;
+
+var quizQuestions = {
+    "Commonly used data types DO Not Include: ": ["1. strings","2. booleans","3. alerts","4. numbers",2],
+    "Arrays in JavaScript can be used to store _____.": ["1. numbers and strings","2. other arrays","3. booleans","4. all of the above",3],
+    "String values must be enclosed within ______ when being assigned to variables": ["1. commas","2. curly brackets", "3. quotes", "4. parenthesis",2]
+}
+
+var questionsEl = document.getElementById("questions");
+var answersEl = document.getElementById("answers");
+
+function loadQuestion(currentQuestion) {
+    var displayQuestion = Object.keys(quizQuestions)[currentQuestion];
+    questionsEl.innerText = displayQuestion;
+}
+
+function loadAnswers(currentQuestion) {
+    var choices = quizQuestions[Object.keys(quizQuestions)[currentQuestion]];
+    answersEl.innerHTML = "";
+    for (var i = 0; i < choices.length - 1; i++) {
+        var createButton = document.createElement("button");
+        var buttonText = document.createTextNode(choices[i]);
+        createButton.appendChild(buttonText);
+        createButton.addEventListener("click",function() {
+
+        })
+        answersEl.appendChild(createButton);
+
+    };
+}
+
+loadQuestion(currentQuestion);
+loadAnswers(currentQuestion);
+
 countdown();
+
