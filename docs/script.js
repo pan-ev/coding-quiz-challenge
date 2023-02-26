8bb5220cb0890561d8d5856bc5b03fc027db3f63
epan <epan616@gmail.com>
2023-02-26 02:11:52 -0500

    Add logic for answering questions and changing screens

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,4 @@
+// Countdown Code
 var countdownEl = document.getElementById("countdown");
 
 var countdownTime = 75;
@@ -6,41 +7,103 @@ function countdown() {
     var setTime = setInterval(function() {
         countdownTime--;
         countdownEl.textContent = countdownTime;
+        if (countdownTime == 0) {
+            clearQuizStart();
+            clearResults();
+            clearQuiz();
+            displayQuizFinished();
+            clearInterval(setTime);
+            finalScoreEl.innerText = "Your final score is " + scoreCounter;
+        }
     }, 1000)
 }
 
+//------------------------------------------------------------
+
 var quizStartEl = document.getElementById("quizStart");
+var quizStartButton = document.getElementById("startQuizButton");
 var quizEl = document.getElementById("quiz");
 var quizFinishedEl = document.getElementById("quizFinished");
 var showHighScoresEl = document.getElementById("showHighScores");
+var viewHighScoresEl = document.getElementById("highscore");
+var goBackButton = document.getElementById("returnToStart");
+var clearScoresButton = document.getElementById("clearScores");
+var results = document.getElementById("results");
+var correctAnswerEl = document.getElementById("correctAnswer");
+var wrongAnswerEl = document.getElementById("wrongAnswer");
+var submitScoreEl = document.getElementById("submitScore");
+var finalScoreEl = document.getElementById("finalScore");
 
-// Functions to clear Page Sections
+var storedHighScores = [];
+
+storedHighScores = JSON.parse(localStorage.getItem("highScoreList"));
+
+// Starting Screen
+function init() {
+    clearQuiz();
+    clearShowHighScores();
+    clearQuizFinished();
+    clearResults();
+}
+init();
+// ---------------------
+
+// Functions to Clear Sections
 function clearQuizStart() {
     quizStartEl.style.display = "none";
 }
 
-function clearQuiz() {
-    quizEl.style.display = "none";
+function clearResults() {
+    results.style.display = "none";
 }
 
 function clearQuizFinished() {
     quizFinishedEl.style.display = "none";
 }
 
+function clearQuiz() {
+    quizEl.style.display = "none";
+}
+
 function clearShowHighScores() {
     showHighScoresEl.style.display = "none";
 }
 
+// Functions to Display Sections
+function displayQuiz() {
+    quizEl.style.display = "flex";
+
+}
+function displayQuizStart() {
+    quizStartEl.style.display = "flex";
+}
+
+function displayResult() {
+    results.style.display = "block";
+}
+
+function displayQuizFinished() {
+    quizFinishedEl.style.display = "flex";
+}
+
+function displayShowHighScores() {
+    showHighScoresEl.style.display = "block";
+}
+
+
+
 var currentQuestion = 0;
+var scoreCounter = 0;
+
+
+var questionsEl = document.getElementById("questions");
+var answersEl = document.getElementById("answers");
 
 var quizQuestions = {
     "Commonly used data types DO Not Include: ": ["1. strings","2. booleans","3. alerts","4. numbers",2],
     "Arrays in JavaScript can be used to store _____.": ["1. numbers and strings","2. other arrays","3. booleans","4. all of the above",3],
     "String values must be enclosed within ______ when being assigned to variables": ["1. commas","2. curly brackets", "3. quotes", "4. parenthesis",2]
 }
-
-var questionsEl = document.getElementById("questions");
-var answersEl = document.getElementById("answers");
 
 function loadQuestion(currentQuestion) {
     var displayQuestion = Object.keys(quizQuestions)[currentQuestion];
@@ -54,16 +117,64 @@ function loadAnswers(currentQuestion) {
         var createButton = document.createElement("button");
         var buttonText = document.createTextNode(choices[i]);
         createButton.appendChild(buttonText);
-        createButton.addEventListener("click",function() {
-
-        })
+        createButton.addEventListener("click",checkAnswer(i, choices))
         answersEl.appendChild(createButton);
-
     };
 }
 
-loadQuestion(currentQuestion);
-loadAnswers(currentQuestion);
+function checkAnswer(i, choicesArray) {
+    return function() {
+        console.log(i);
+        var chosenAnswer = i;
+        var correctAnswer = choicesArray[choicesArray.length-1];
+    
+        if (chosenAnswer === correctAnswer) {
+            displayResult();
+            correctAnswerEl.style.display = "inline";
+            wrongAnswerEl.style.display = "none";
+            scoreCounter += 1;
+        } else {
+            displayResult();
+            correctAnswerEl.style.display = "none";
+            wrongAnswerEl.style.display = "inline";
+            countdownTime -= 10;
+        }
 
-countdown();
+        if (currentQuestion < Object.keys(quizQuestions).length - 1) {
+            currentQuestion += 1;
+
+            loadQuestion(currentQuestion);
+            loadAnswers(currentQuestion);
+        } else {
+            clearQuiz();
+            countdownTime = 1;
+        }
+    }
+}
+
+quizStartButton.addEventListener("click",function() {
+    clearQuizStart();
+    currentQuestion = 0;
+    scoreCounter = 0;
+    displayQuiz();
+    loadQuestion(currentQuestion);
+    loadAnswers(currentQuestion);
+    countdownTime = 75;
+    countdown();
+});
+
+submitScoreEl.addEventListener("click",function(event) {
+    event.preventDefault();
+    var playerInitialsEl = document.getElementById("playerInitials").value;
+    storedHighScores.push(playerInitialsEl + " - " + scoreCounter);
+    scoreCounter = 0;
+    localStorage.setItem("highScoreList", JSON.stringify(storedHighScores))
+    console.log(storedHighScores);
+    init();
+    displayQuizStart();
+});
+
+goBackButton.addEventListener("click",function() {
+    displayQuizStart();
+});
 
